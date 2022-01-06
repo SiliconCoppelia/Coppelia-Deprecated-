f136286af779c01951be2092c2d4616cf06c3bb1
BanjiBear <70761188+BanjiBear@users.noreply.github.com>
2022-01-06 19:04:50 +0800

    Update Concatenate.java

--- SiliconCopellia/src/Concatenate.java
+++ SiliconCopellia/src/Concatenate.java
@@ -1,2 +1,42 @@
+import dimensions.*;
+
 public class Concatenate {
+
+    public static void main(String[] args){
+        System.out.println("Please enter all the values separated by enters: \n");
+
+        Ethics eth = new Ethics();
+        Affordance aff = new Affordance();
+        Relavance rel = new Relavance();
+        Valence val = new Valence();
+        Involvement invl = new Involvement();
+        Distance dist = new Distance();
+        UserIntension usr = new UserIntension();
+
+        StringBuffer sent = new StringBuffer("I find you ");
+        if((eth.num >= 0.5 && aff.num >= 0.5) || (eth.num < 0.5 && aff.num < 0.5)){
+            sent = sent.append(eth.compare()).append(" and ").append(aff.compare()).append(", therefore, ");
+        }
+        else{
+            sent = sent.append(eth.compare()).append(" but ").append(aff.compare()).append(", therefore, ");
+        }
+
+        if(invl.num >= dist.num && val.num < 0.5){
+            sent.append(invl.compare()).append(" but ").append(val.compare()).append(" ").append(rel.compare()).append(". ");
+        }
+        else if(invl.num >= dist.num && val.num >= 0.5){
+            sent.append(invl.compare()).append(" and ").append(val.compare()).append(" ").append(rel.compare()).append(". ");
+        }
+        else if(invl.num < dist.num && val.num >= 0.5){
+            sent.append(dist.compare()).append(" but ").append(val.compare()).append(" ").append(rel.compare()).append(". ");
+        }
+        else if(invl.num < dist.num && val.num < 0.5){
+            sent.append(dist.compare()).append(" and ").append(val.compare()).append(" ").append(rel.compare()).append(". ");
+        }
+
+        sent.append(usr.compare());
+
+        System.out.println(sent.toString());
+
+    }
 }
