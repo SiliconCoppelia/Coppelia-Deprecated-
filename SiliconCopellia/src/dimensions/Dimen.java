84d35b9184990dad10e446d43f0e210f2d611614
ZHANG Yujing <74478753+Yookivivi@users.noreply.github.com>
2022-01-07 16:00:50 +0800

    Update Dimen.java

--- SiliconCopellia/src/dimensions/Dimen.java
+++ SiliconCopellia/src/dimensions/Dimen.java
@@ -1,8 +1,15 @@
 package dimensions;
 
 
+import java.util.Scanner;
+
 public abstract class Dimen {
     private float num;
     public abstract String compare();
-    public abstract float readInput();
+
+    public float readInput(){
+        Scanner scan = new Scanner(System.in);
+        this.num = scan.nextFloat();
+        return this.num;
+    }
 }
