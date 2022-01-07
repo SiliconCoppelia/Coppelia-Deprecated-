99d7373d6f23fcd11f5bc36c3499cd6bf189481b
ZHANG Yujing <74478753+Yookivivi@users.noreply.github.com>
2022-01-07 16:30:46 +0800

    bugs

--- SiliconCopellia/src/dimensions/Distance.java
+++ SiliconCopellia/src/dimensions/Distance.java
@@ -20,11 +20,11 @@ public class Distance extends Dimen{
 
 
     public String compare(){
-        if(this.num < 0.2 && this.num >= 0) this.index = 0;
-        else if (this.num < 0.4 && this.num >= 0.2) this.index = 1;
+        if(this.num < 0.2 && this.num >= 0) this.index = 4;
+        else if (this.num < 0.4 && this.num >= 0.2) this.index = 3;
         else if (this.num < 0.6 && this.num >= 0.4) this.index = 2;
-        else if (this.num < 0.8 && this.num >= 0.6) this.index = 3;
-        else  this.index = 4;
+        else if (this.num < 0.8 && this.num >= 0.6) this.index = 1;
+        else  this.index = 0;
 
         return this.affordance[this.index];
     }
