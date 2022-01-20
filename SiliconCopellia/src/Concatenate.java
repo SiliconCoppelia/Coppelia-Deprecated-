0d45c74a9a2e381b70525a701d747ba617d6c9fe
BanjiBear <70761188+BanjiBear@users.noreply.github.com>
2022-01-20 15:16:56 +0800

    update the parameters to pass to other classes, not done yet

--- SiliconCopellia/src/Concatenate.java
+++ SiliconCopellia/src/Concatenate.java
@@ -1,46 +1,73 @@
 import dimensions.*;
-import java.util.Locale;
-
+import java.util.Scanner;
+import java.lang.*;
 
 public class Concatenate {
 
+    private static StringBuffer inputBuffer = new StringBuffer();
+    Scanner scan = new Scanner(System.in);
+    String strInput = scan.nextLine();
+
+    private static double ETHICS;
+    private static double[] affordanceFeatures;
+    private static double[] RELEVANCE;      /* form: [eth, affF1, affF2, affF3] */
+    private static double[] VALENCE;        /* form: [eth, affF1, affF2, affF3] */
+    private static double INVOLVEMENT;
+    private static double DISTANCE;
+    private static double USR_INTENTION;
+
+    private static StringBuffer sent = new StringBuffer("I find you ");
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Please enter all the values separated by enters: ");
 
-        Ethics eth = new Ethics();
-        Affordance aff = new Affordance();
-        Relavance rel = new Relavance();
-        Valence val = new Valence();
-        Involvement invl = new Involvement();
-        Distance dist = new Distance();
-        UserIntension usr = new UserIntension();
+        // Step 1: greetings from Copéllia
+        greetings();
 
-        StringBuffer sent = new StringBuffer("I find you ");
-        if((eth.num >= 0.5 && aff.num >= 0.5) || (eth.num < 0.5 && aff.num < 0.5)){
-            sent = sent.append(eth.compare()).append(" and ").append(aff.compare()).append(", therefore, ");
-        }
-        else{
-            sent = sent.append(eth.compare()).append(" but ").append(aff.compare()).append(", therefore, ");
-        }
+        // Step 2: Prompt all the input values and specifies the format explicitly
+        // Specifies input format and corresponding
+        System.out.println("Please type in the values separated by spaces:\nValues for Ethics and its Relevance and Valence, (e.g. 0.63 0.66 0.435): ");
+        System.out.println("Values for Affordance-feature 1 and its Relevance and Valence, (e.g. 0.63 0.66 0.435): ");
+        System.out.println("Values for Affordance-feature 2 and its Relevance and Valence, (e.g. 0.63 0.66 0.435): ");
+        System.out.println("Values for Affordance-feature 3 and its Relevance and Valence, (e.g. 0.63 0.66 0.435): ");
 
-        if(invl.num >= dist.num && val.num < 0.5){
-            sent.append(invl.compare()).append(" but ").append(val.compare()).append(" ").append(rel.compare()).append(". ");
-        }
-        else if(invl.num >= dist.num && val.num >= 0.5){
-            sent.append(invl.compare()).append(" and ").append(val.compare()).append(" ").append(rel.compare()).append(". ");
-        }
-        else if(invl.num < dist.num && val.num >= 0.5){
-            sent.append(dist.compare()).append(" but ").append(val.compare()).append(" ").append(rel.compare()).append(". ");
-        }
-        else if(invl.num < dist.num && val.num < 0.5){
-            sent.append(dist.compare()).append(" and ").append(val.compare()).append(" ").append(rel.compare()).append(". ");
-        }
+        System.out.println("Please type in the value for Involvement, (e.g. 0.63): ");
+        System.out.println("Please type in the value for Distance, (e.g. 0.63): ");
+        System.out.println("Please type in the value for Use Intention, (e.g. 0.63): ");
 
-        sent.append(usr.compare());
+
+        Ethics eth = new Ethics(ETHICS);
+        Affordance aff = new Affordance(RELEVANCE, affordanceFeatures);
+        //Relevance rel = new Relevance();
+        Valence val = new Valence(RELEVANCE, VALENCE);
+        Involvement invl = new Involvement(INVOLVEMENT);
+        Distance dist = new Distance(DISTANCE);
+        UserIntention usr = new UserIntention(USR_INTENTION, Valence.vectorCalculator());
+
         System.out.println(sent.toString());
 
-        TextToSpeech tts=new TextToSpeech(sent.toString());
+        //StringBuffer cmd = new StringBuffer("say ");
+        //cmd.append(sent.toString());
+        //Process pr = Runtime.getRuntime().exec(cmd.toString());
 
+    }
 
+    private static void greetings() throws Exception{
+        System.out.println("Hi there, I'm Copéllia, what are we up to today?\n");
+        Runtime.getRuntime().exec("say Hi there, I'm Copéllia, what are we up to today?");
+
+        Scanner scan = new Scanner(System.in);
+        String str = scan.nextLine();
+        scan.close();
+        if(str.toLowerCase().contains("date")){
+            Runtime.getRuntime().exec("say Let me think...");
+            System.out.println("Let me think...\n");
+        }
+        else{
+            System.out.println("I'm sorry. " +
+                    "I don't yet have the ability to understand such a difficult command\n");
+            Runtime.getRuntime().exec("say I'm sorry. " +
+                    "I don't yet have the ability to understand such a difficult command");
+            System.exit(0);
+        }
     }
 }
