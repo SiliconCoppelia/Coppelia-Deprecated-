10b2f53ce40fb990962e7963de9fc7cc31c9bf64
ZHANG Yujing <74478753+Yookivivi@users.noreply.github.com>
2022-01-20 13:37:04 +0800

    Update Distance.java

--- SiliconCopellia/src/dimensions/Distance.java
+++ SiliconCopellia/src/dimensions/Distance.java
@@ -6,11 +6,11 @@ public class Distance extends Dimen{
     private int index;
     public float num;
     private String[] affordance = {
-            "you are weird.",
-            "you make me feel uneasy and nervous",
-            "sometimes you give me feelings of discomfort",
-            "I don't really like you",
-            "you are not my friend"
+            "I extremely have reservations about you",
+            "I have much resevations about you",
+            "I have reservations about you",
+            "I somewhat have reservations about you",
+            "I hardly have reservations about you"
     };
 
     public Distance() throws Exception{
