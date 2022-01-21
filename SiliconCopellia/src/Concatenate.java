8bdaf67a52220c177820afa6b18dbf0bd570f1c4
BanjiBear <70761188+BanjiBear@users.noreply.github.com>
2022-01-21 23:20:19 +0800

    Add spaces between words

--- SiliconCopellia/src/Concatenate.java
+++ SiliconCopellia/src/Concatenate.java
@@ -119,7 +119,7 @@ public class Concatenate {
         sent.append(", which I think is very important for deciding whether or not to go out with you, thus, ");
         sent.append(val.compare()).append(". Therefore, ").append(usr.compare());
         if(invl.num > dist.num && val.num < 0) sent.append(" ").append(invl.compare()).append(". However, no hard feelings but I'm sorry that you might need to find someone to date with.");
-        else if(invl.num > dist.num && val.num > 0) sent.append(invl.compare()).append(", you are really a good person. I'm sure we will get along very well!");
+        else if(invl.num > dist.num && val.num > 0) sent.append(" ").append(invl.compare()).append(", you are really a good person. I'm sure we will get along very well!");
         else if(invl.num <= dist.num && val.num > 0) sent.append(" Though ").append(dist.compare()).append(", I will still date you. Let's give it a try.");
         else if(invl.num <= dist.num && val.num < 0) sent.append(" ").append(dist.compare()).append(". Please go find someone else!");
     }
