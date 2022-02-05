96a9c25e24bfd8e47702cff0a897387f79cac690
Tommy CHEN <70761188+BanjiBear@users.noreply.github.com>
2022-02-05 21:13:45 +0800

    Update Affordance.java

--- SiliconCopellia/src/dimensions/Affordance.java
+++ SiliconCopellia/src/dimensions/Affordance.java
@@ -1,119 +1,125 @@
-package dimensions;
+import dimensions.*;
+import java.util.*;
+import java.lang.*;
+import javax.swing.*;
 
-//import java.util.Scanner;
+import static java.lang.Integer.parseInt;
 
-public class Affordance{
+public class Concatenate {
 
-    private int[] index;
+    private static Scanner scan = new Scanner(System.in);
 
-    private int[] affordanceFeatures;
-    private Relevance relvance;
-    private Valence[] valences;
-    private String[] realFeatures=new String[2];
-    //private String[] affordance = {};
-    private String[] age=new String[]{
-            "you are a child",
-            "you are an adolescent",
-            "young",
-            "mature",
-            "old"
-    };
+    private static double ETHICS;
+    private static int[] affordanceFeatures = new int[3];
+    //private static double[] RELEVANCE = new double[4];      /* form: [eth, affF1, affF2, affF3] */
+    //private static double[] VALENCE = new double[4];        /* form: [eth, affF1, affF2, affF3] */
+    //private static double INVOLVEMENT;
+    //private static double DISTANCE;
+    //private static double USR_INTENTION;
+    private static int num = 0;
+    private static int index = 0;
 
-    private String[] assetStatus=new String[]{
-            "poor",
-            "relatively poor",
-            "not wealthy but not poor as well",
-            "relatively wealthy",
-            "wealthy"
-    };
+    private static StringBuffer sent = new StringBuffer("You are ");
 
-    /* it may conflict with aesthetic (TBD)
-    private String[] attractiveness=new String[]{
-            "ugly",
-            "unsightly",
-            "average",
-            "attractive",
-            "beautiful"
-    };*/
+    public static void main(String[] args) throws Exception {
 
-    private String[] condition=new String[]{
-            "you have no pets",
-            "you keep more than 3 pets",
-            "you keep my ideal number of pets(1 or 2)"
-    };
+        // Step 1: greetings from Coppélia
+        greetings();
 
-    public Affordance(int[] affordanceFeatures){
-        this.affordanceFeatures=affordanceFeatures.clone();
-        //this.relvance=new Relevance[](new double[]{0.8, 0.4, 0.2});
-        this.valences=new Valence[3];
+        // Step 2: self-introduction from the agent
+        usrIntro();
+
+        // Step 3: Create all object and pass parameters for processing
+        Affordance aff = new Affordance(affordanceFeatures);
+        Valence val = new Valence(affordanceFeatures);
+
+        // Step 4: Sentence formulation
+        sentenceFormulation(aff, val);
+        System.out.println(sent.toString());
+
+        System.exit(0);
     }
 
-    public Valence[] getValences(){
-        return this.valences;
+    private static void greetings() throws Exception{
+        System.out.println("Hi, I'm Coppélia.\nI want to go on a date with you.\n");
+
+        // Currently analyze the agent by prompting self-introduction
     }
 
-    public String[] compare(){
-        for(int i = 1; i < index.length; i++){
-            switch(index[i]){
-                case 1:
-                    if(this.affordanceFeatures[0] < 14){
-                        realFeatures[i-1]=age[0];
-                        this.valences[i-1]=new Valence(0.35-0.02*Math.abs(14-affordanceFeatures[0]));
-                    }else if(this.affordanceFeatures[0] < 21){
-                        realFeatures[i-1]=age[1];
-                        this.valences[i-1]=new Valence(0.7-0.05*Math.abs(21-affordanceFeatures[0]));
-                    }else if(this.affordanceFeatures[0]<33){//21-33
-                        realFeatures[i-1]=age[2];
-                        this.valences[i-1]=new Valence(1-0.05*Math.abs(27-affordanceFeatures[0]));
-                    }else if(this.affordanceFeatures[0]<55){
-                        realFeatures[i-1]=age[3];
-                        this.valences[i-1]=new Valence(0.7-0.02*Math.abs(33-affordanceFeatures[0]));
-                    }else {
-                        realFeatures[i-1]=age[4];
-                        this.valences[i-1]=new Valence(0.26-0.005*Math.abs(55-affordanceFeatures[0]));
-                    }
+    private static void usrIntro(){
 
-                    break;
-                case 2:
-                    if(this.affordanceFeatures[1] < 2000){
-                        realFeatures[i-1]=assetStatus[0];
-                        this.valences[i-1]=new Valence(0.2-0.0001*Math.abs(2000-affordanceFeatures[1]));
-                    }else if(this.affordanceFeatures[1] < 5000){
-                        realFeatures[i-1]=assetStatus[1];
-                        this.valences[i-1]=new Valence(0.4-0.00005*Math.abs(5000-affordanceFeatures[1]));
-                    }else if(this.affordanceFeatures[1]<10000){
-                        realFeatures[i-1]=assetStatus[2];
-                        this.valences[i-1]=new Valence(0.6-0.00004*Math.abs(10000-affordanceFeatures[1]));
-                    }else if(this.affordanceFeatures[1]<50000){
-                        realFeatures[i-1]=assetStatus[3];
-                        this.valences[i-1]=new Valence(0.8-0.000005*Math.abs(50000-affordanceFeatures[1]));
-                    }else {
-                        realFeatures[i-1]=assetStatus[4];
-                        this.valences[i-1]=new Valence(Math.min(1,0.8+0.000005*Math.abs(50000-affordanceFeatures[1])));
-                    }
+        JFrame jFrame = new JFrame(); // https://www.delftstack.com/howto/java/java-pop-up-window/
+        affordanceFeatures[0] = parseInt(JOptionPane.showInputDialog(jFrame, "What is your age?"));
+        affordanceFeatures[1] = parseInt(JOptionPane.showInputDialog(jFrame, "What is your monthly income?"));
+        affordanceFeatures[2] = parseInt(JOptionPane.showInputDialog(jFrame, "How many pets do you have?"));
+        /*
+        System.out.println(getMessage);
+        for(int i = 0; i < affordanceFeatures.length; i++){
+            System.out.println(affordanceFeatures[i]);
+        }
+        */
+    }
 
-                    break;
-                case 3:
-                    if(this.affordanceFeatures[2]==0){
-                        realFeatures[i-1]=condition[0];
-                        this.valences[i-1]=new Valence(0.2);
-                    }else if(this.affordanceFeatures[2]>=3) {
-                        realFeatures[i-1]=condition[1];
-                        this.valences[i-1]=new Valence(Math.max(0.2, 1-0.1*(this.affordanceFeatures[2]-2)));
-                    }else{
-                        realFeatures[i-1]=condition[2];
-                        this.valences[i-1]=new Valence(1);
-                    }
-
-                    break;
-                default:
-                    break;
+    private static void sentenceFormulation(Affordance aff, Valence val){
+        countPos(val);
+        if(num == 3){
+            sent.append("not only ").append(aff.compare()[0]).append(" and ").append(aff.compare()[1]).append(" but also keeping ");
+            if(affordanceFeatures[2] == 1) sent.append(String.valueOf(affordanceFeatures[2])).append(" pet.");
+            else sent.append(String.valueOf(affordanceFeatures[2])).append(" pets.");
+        }
+        else if(num == 0){
+            sent.append(aff.compare()[0]).append(", ").append(aff.compare()[1]);
+            //System.out.print(affordanceFeatures[2]); // For debugging
+            if(affordanceFeatures[2] == 0) sent.append(", and you don't keep pets.");
+            else sent.append(", and you keep ").append(String.valueOf(affordanceFeatures[2])).append(" pets");
+        }
+        else if(num == 2){
+            if(negIndex(val) == 0){
+                sent.append(aff.compare()[1]).append(" and you keep ");
+                if(affordanceFeatures[2] == 1) sent.append(String.valueOf(affordanceFeatures[2])).append(" pet");
+                else sent.append(String.valueOf(affordanceFeatures[2])).append(" pets");
+                sent.append(" but you are ").append(aff.compare()[0]);
+            }
+            else if(negIndex(val) == 1){
+                sent.append(aff.compare()[0]).append(" and you keep ");
+                if(affordanceFeatures[2] == 1) sent.append(String.valueOf(affordanceFeatures[2])).append(" pet");
+                else sent.append(String.valueOf(affordanceFeatures[2])).append(" pets");
+                sent.append(" but you are ").append(aff.compare()[1]);
+            }
+            else if(negIndex(val) == 2){
+                sent.append(aff.compare()[0]).append(" and ").append(aff.compare()[1]).append(" but you ");
+                if(affordanceFeatures[2] == 0) sent.append("don't keep pets");
+                else sent.append("keep ").append(String.valueOf(affordanceFeatures[2])).append(" pets");
             }
         }
+        else if(num == 1){
+            if(posIndex(val) == 0){
+                
+            }
+        }
+    }
 
+    private static int countPos(Valence val){
+        for(int i = 0; i < val.ValOfFeatures().length; i++){
+            if(val.ValOfFeatures()[i] > 0) num++;
+        }
+        //System.out.print(num); System.out.println("\n"); // For debugging
+        return num;
+    }
 
-        return realFeatures;
+    private static int posIndex(Valence val){
+        for(int i = 0; i < val.ValOfFeatures().length; i++){
+            if(val.ValOfFeatures()[i] > 0) index = i;
+        }
+
+        return index;
+    }
+    private static int negIndex(Valence val){
+        for(int i = 0; i < val.ValOfFeatures().length; i++){
+            if(val.ValOfFeatures()[i] < 0) index = i;
+        }
+        //System.out.print(index); //For debugging
+
+        return index;
     }
 }
-
-// --------------------------------------------------
