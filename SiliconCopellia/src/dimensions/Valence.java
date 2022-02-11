058df07a362ab0ed0f51922e3b8da8e3fc0b0b36
Tommy CHEN <70761188+BanjiBear@users.noreply.github.com>
2022-02-11 18:29:08 +0800

    Update Valence.java

--- SiliconCopellia/src/dimensions/Valence.java
+++ SiliconCopellia/src/dimensions/Valence.java
@@ -4,41 +4,35 @@ package dimensions;
 
 public class Valence{
 
-    private int index;
-    public double num;
-    private int[] rel_index;
-    private double val;
-    private double[] rel;
-    private String[] valence = {
-            "I am certain that I can fulfill your goal",
-            "I have positive expectations in fulfilling your goal",
-            "I have negative expectations in fulfilling your goal",
-            "I am certain that I cannot help you with your goal"
-            };
+    private int[] valOfFeatures = new int[3];
+    private int[] affordanceFeatures;
 
-    /*
-    public Valence(int[] index,  double[] Relevance, double[] VALENCE){
-        this.rel_index=index;
-        this.val=VALENCE;
-        this.rel=Relevance;
-        this.num=(this.rel[this.rel_index[0]]*this.val[0]+
-                 this.rel[this.rel_index[1]]*this.val[1]+
-                 this.rel[this.rel_index[2]]*this.val[2])/3;
-    }*/
-    public Valence(double VALENCE){
-        this.val=VALENCE;
+    public Valence(int[] affordanceFeatures){
+        this.affordanceFeatures=affordanceFeatures;
     }
 
-    public double getValence(){
-        return this.num;
-    }
-
-    public String compare(){
-        if(this.num < -0.5) this.index = 3;
-        else if (this.num < 0) this.index = 2;
-        else if (this.num < 0.5) this.index = 1;
-        else  this.index = 0;
-
-        return this.valence[this.index];
+    public int[] ValOfFeatures(){
+        for(int i = 0; i < this.affordanceFeatures.length; i++){
+            switch (i){
+                case 0:
+                    if(this.affordanceFeatures[i] < 14 || this.affordanceFeatures[i] > 55) valOfFeatures[i] = -1;
+                    else if(this.affordanceFeatures[i] > 14 && this.affordanceFeatures[i] < 21) valOfFeatures[i] = 1;
+                    else if(this.affordanceFeatures[i] > 21 && this.affordanceFeatures[i] < 33) valOfFeatures[i] = 2;
+                    else if(this.affordanceFeatures[i] > 33 && this.affordanceFeatures[i] < 55) valOfFeatures[i] = 1;
+                    break;
+                case 1:
+                    if(this.affordanceFeatures[i] < 2000) valOfFeatures[i] = -1;
+                    else if(this.affordanceFeatures[i] < 5000) valOfFeatures[i] = -1;
+                    else if(this.affordanceFeatures[i] < 10000) valOfFeatures[i] = 1;
+                    else if(this.affordanceFeatures[i] < 50000) valOfFeatures[i] = 1;
+                    else valOfFeatures[i] = 2;
+                    break;
+                case 2:
+                    if(this.affordanceFeatures[i] == 1 || this.affordanceFeatures[i] == 2) valOfFeatures[i] = 2;
+                    else valOfFeatures[i] = -1;
+                    break;
+            }
+        }
+        return valOfFeatures;
     }
 }
