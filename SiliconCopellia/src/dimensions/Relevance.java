2904ff9f2a5f56023e598070c7db549412f7174e
Tommy CHEN <70761188+BanjiBear@users.noreply.github.com>
2022-02-12 12:19:27 +0800

    Update Relevance.java

--- SiliconCopellia/src/dimensions/Relevance.java
+++ SiliconCopellia/src/dimensions/Relevance.java
@@ -30,31 +30,4 @@ public class Relevance {
 
         return level;
     }
-    /*public int[] top2_index(){
-        int[] temp=new int[]{0, 2, 3};
-        int minIndex=1;
-        double minNum=relevance[1];
-        for(int i=2; i<4; i++){
-            if(relevance[i]<minNum){
-                minNum=relevance[i];
-                minIndex=i;
-            }
-        }
-        switch(minIndex){
-            case 1:
-                temp=new int[]{0, 2, 3};
-                break;
-            case 2:
-                temp=new int[]{0, 1, 3};
-                break;
-            case 3:
-                temp=new int[]{0, 1, 2};
-                break;
-            default:
-                break;
-        }
-        return temp;
-    }*/
-
-
 }
